78bed5797febfd322cb18841a98ac0eb281cce14
Alekc74 <107016891+Alekc74@users.noreply.github.com>
2022-06-07 11:09:24 +0800

    Add files via upload

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+You can use the code however you want, but any damage caused by this program is on your own risk. The code is free to distribute as long as you link to the original :)
---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "=================="
+echo "Bash script for routersploit installation on termux"
+echo "Keep your phone alive and try not to use it while installing"
+echo "=================="
+sleep 3
+apt update -y && apt upgrade -y
+apt install autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk git grep gzip libtool make patch perl sed silversearcher-ag tar wget pkg-config python clang libcrypt libffi git openssl libsodium -y
+python -m pip install wheel
+pip install rust
+export CARGO_BUILD_TARGET=arm7-linux-androideabi && pip install cryptography==2.8
+SODIUM_INSTALL=system pip install pynacl
+git clone https://github.com/threat9/routersploit
+mv routersploit $HOME/
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+echo "Done!"
