e51768b3a8312e0e46b313888f092240270582ad
Alekc74 <107016891+Alekc74@users.noreply.github.com>
2022-06-07 12:10:23 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,6 @@ echo "=================="
 sleep 3
 apt update -y && apt upgrade -y
 apt install autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk git grep gzip libtool make patch perl sed silversearcher-ag tar wget pkg-config python clang libcrypt libffi git openssl libsodium -y
-python -m pip install wheel
 SODIUM_INSTALL=system pip install pynacl
 git clone https://github.com/threat9/routersploit
 mv routersploit $HOME/
